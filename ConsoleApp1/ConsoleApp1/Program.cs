bf526de202f6b7d0c027503c245044d28c48d83b
Victor <38391733+Essvitex@users.noreply.github.com>
2019-09-11 10:59:04 +0500

    adding cs code with bin and objs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Sample1
+    {
+        
+    }
+
+    class Program
+    {
+        static void F()
+        {
+            i = 1;
+            Console.Write(i);
+        }
+        static int i = 0;
+
+        static void Main(string[] args)
+        {
+            F();
+            Console.ReadKey();
+        }
+
+        private static string Str(string str)
+        {
+            int point = str.Length / 2;
+            return (str.Substring(0, point)).Replace(" ", string.Empty);
+        }
+
+    }
+}
